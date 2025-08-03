a90376a5521471ce1419af880dd9d4d039d71bb3
crazystorm83 <crazystorm83@gmail.com>
2025-08-03 21:02:19 +0900

    ESLint 규칙 추가 및 설정 개선

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,42 @@ module.exports = {
   ],
   rules: {
     // 필요에 따라 규칙 추가
+
+    // Possible Problems 
+    'constructor-super': 'on',
+
+    'for-direction': 'on',
+    'getter-return': 'on',
+
+    'no-cond-assign': 'on',
+    'no-cond-assign': 'on',
+    'no-constant-binary-expression': 'on',
+    'no-constant-condition': 'on',
+    'no-constructor-return': 'on',
+    'no-dupe-class-members': 'on',
+    'no-dupe-else-if': 'on',
+    'no-dupe-keys': 'on',
+    'no-duplicate-case': 'on',
+    'no-duplicate-imports': 'on',
+    'no-ex-assign': 'on',
+    'no-fallthrough': 'on',
+    'no-func-assign': 'on',
+    'no-import-assign': 'on',
+    'no-inner-declarations': 'on',
+    'no-self-assign': 'on',
+    'no-self-compare': 'on',
+    'no-setter-return': 'on',
+    'no-this-before-super': 'on',
+    'no-unsafe-negation': 'on',
+    'no-unsafe-optional-chaining': 'on',
+    'no-unused-private-class-members': 'on',
+    'no-unused-vars': 'on',
+    'no-use-before-define': 'on',
+    'require-atomic-updates': 'on',
+    'use-isnan': 'on',
+    'valid-typeof': 'on',
+
+    // Suggestions
+    'block-scoped-var': 'on',
   }
 }; 

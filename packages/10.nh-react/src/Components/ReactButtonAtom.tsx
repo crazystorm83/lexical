71f1dd5b9e50c4942a9ace25c202d8307c32a6ec
crazystorm83 <crazystorm83@gmail.com>
2025-08-10 01:04:46 +0900

    패키지 스크립트 수정: build 스크립트를 변경하여 개발 환경에서 프로덕션 빌드로 전환. ReactButtonAtom 컴포넌트의 onClick 타입 정의를 수정하고, 불필요한 index.ts 파일을 삭제하여 코드 정리.

--- packages/10.nh-react/src/Components/ReactButtonAtom.tsx
+++ packages/10.nh-react/src/Components/ReactButtonAtom.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 
 interface IReactButtonAtomProps {
     children: React.ReactNode;
-    onClick: (e: React.MouseEvent<HtmlButtonElement>) => void;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const ReactButtonAtom = memo((props: IReactButtonAtomProps) => {
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Sinuous 라이브러리 샘플 엔트리 포인트
-
-export function hello(name: string = 'sinuous'): string {
-  return `hello, ${name}!`;
-} 
